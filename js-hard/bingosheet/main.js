6590fa83ae17af508e34cf21a27312c719cbe12e
Amattodev <takuhuyu44@gmail.com>
2024-11-09 15:58:25 +0900

    ビンゴシート修正完了

--- js-hard/bingosheet/main.js
+++ js-hard/bingosheet/main.js
@@ -1,22 +1,29 @@
 let bingoSheet = [];
+let usedNumbers = [];
 let answerNumbers = [];
 const table = document.getElementById("view");
 const hitNum = document.getElementById("hitNum");
-//5x5のビンゴシートを作成
-for (let i = 0; i < 5; i++) {
-  bingoSheet[i] = [];
-  for (let j = 0; j < 5; j++) {
+
+for (let row = 0; row < 5; row++) {
+  bingoSheet[row] = [];
+  for (let column = 0; column < 5; column++) {
+    let num;
     do {
-      num = Math.floor(Math.random() * 99);
-      bingoSheet[i][j] = num;
-    } while (bingoSheet.includes(num));
-    if (i === 2 && j === 2) {
-      bingoSheet[i][j] = "Free";
+      let min = column * 15 + 1; //1,16,31,46,61
+      let max = column * 15 + 15; //15,30,45,60,75
+      num = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (usedNumbers.includes(num));
+    bingoSheet[row][column] = num;
+    usedNumbers.push(num);
+    if (row === 2 && column === 2) {
+      bingoSheet[row][column] = "Free";
     }
   }
+  console.log(bingoSheet);
+  console.log(usedNumbers);
 }
 
-//trとtdの要素を作成し、テーブルを表示
+// trとtdの要素を作成し、テーブルを表示
 const displayBingoSheet = () => {
   bingoSheet.forEach((row) => {
     const tr = document.createElement("tr");
@@ -34,10 +41,9 @@ displayBingoSheet();
 hitNum.addEventListener("click", () => {
   let answerNum;
   do {
-    answerNum = Math.floor(Math.random() * 99);
+    answerNum = Math.floor(Math.random() * 75) + 1;
   } while (answerNumbers.includes(answerNum));
   answerNumbers.push(answerNum);
-  console.log(answerNumbers);
   alert(`数字は${answerNum}番です！`);
   const tds = Array.from(table.getElementsByTagName("td"));
   tds.forEach((td) => {
