5128acc5b6dada93d4ed4bd0b82ce6b9df532141
Amattodev <takuhuyu44@gmail.com>
2024-11-14 11:48:36 +0900

    神経衰弱ゲーム完了

--- js-hard/sinkeisuizyaku/main.js
+++ js-hard/sinkeisuizyaku/main.js
@@ -1,44 +1,60 @@
-const panel = document.getElementById("panel");
-let cards = [1, 1, 2, 2, 3, 3, 4, 4];
-let flippedCards = [];
-
-//シャッフルする関数
-const shuffle = () => {
-  for (i = cards.length; 1 < i; i--) {
-    let randomNum = Math.floor(Math.random() * i); //0~7
-    [cards[randomNum], cards[i - 1]] = [cards[i - 1], cards[randomNum]];
-  }
+const gameData = {
+  cards: [1, 1, 2, 2, 3, 3, 4, 4],
+  flipCardsArray: [],
+  cardMatchCount: 0,
 };
-shuffle();
 
-cards.forEach((index) => {
-  const card = document.createElement("div");
-  card.classList.add("card", "back");
-  panel.appendChild(card);
-  card.textContent = cards[index];
-  card.addEventListener("click", () => {
-    if (flippedCards.length < 2) {
-      card.classList.remove("back");
-      card.classList.add("flipped");
-      flippedCards.push(card);
-      setTimeout(checkCards, 1500);
-    }
+const shuffle = () => {
+  for (i = gameData.cards.length; 0 < i; i--) {
+    let randomNum = Math.floor(Math.random() * i); //0~7
+    [gameData.cards[randomNum], gameData.cards[i - 1]] = [
+      gameData.cards[i - 1],
+      gameData.cards[randomNum],
+    ];
+  }
+  flipCards();
+};
+
+const flipCards = () => {
+  gameData.cards.forEach((element) => {
+    //カードに数字を表示
+    const panel = document.getElementById("panel");
+    const card = document.createElement("div");
+    card.classList.add("card", "back");
+    panel.appendChild(card);
+    card.textContent = element;
+    //カードをフリップする
+    card.addEventListener("click", () => {
+      if (gameData.flipCardsArray.length < 2) {
+        card.classList.remove("back");
+        card.classList.add("flipped");
+        gameData.flipCardsArray.push(card);
+        setTimeout(checkCards, 1500);
+      }
+    });
   });
-});
+};
 
 const checkCards = () => {
-  if (flippedCards[0].textContent === flippedCards[1].textContent) {
-    flippedCards[0].classList.add("finish");
-    flippedCards[1].classList.add("finish");
-    console.log(flippedCards[0].textContent, flippedCards[1].textContent);
-    flippedCards = [];
+  if (
+    gameData.flipCardsArray[0].textContent ===
+    gameData.flipCardsArray[1].textContent
+  ) {
+    gameData.flipCardsArray[0].classList.add("finish");
+    gameData.flipCardsArray[1].classList.add("finish");
+    gameData.cardMatchCount += 2;
+    gameData.flipCardsArray = [];
+    if (gameData.cardMatchCount === gameData.cards.length) {
+      console.log("aaa");
+      alert("終了");
+    }
   } else {
-    flippedCards[0].classList.remove("flipped");
-    flippedCards[1].classList.remove("flipped");
-    flippedCards[0].classList.add("back");
-    flippedCards[1].classList.add("back");
-    console.log(flippedCards[0].textContent, flippedCards[1].textContent);
-    flippedCards = [];
+    gameData.flipCardsArray[0].classList.remove("flipped");
+    gameData.flipCardsArray[1].classList.remove("flipped");
+    gameData.flipCardsArray[0].classList.add("back");
+    gameData.flipCardsArray[1].classList.add("back");
+    gameData.flipCardsArray = [];
   }
-  console.log(flippedCards);
 };
+
+shuffle();
