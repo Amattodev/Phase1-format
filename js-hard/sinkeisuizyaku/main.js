0403a507a4a20f339f17cfa277e49095fd6e8ce5
Amattodev <takuhuyu44@gmail.com>
2024-11-14 09:24:07 +0900

    カードの判定処理

--- js-hard/sinkeisuizyaku/main.js
+++ js-hard/sinkeisuizyaku/main.js
@@ -7,9 +7,6 @@ const shuffle = () => {
   for (i = cards.length; 1 < i; i--) {
     let randomNum = Math.floor(Math.random() * i); //0~7
     [cards[randomNum], cards[i - 1]] = [cards[i - 1], cards[randomNum]];
-    console.log(cards);
-    console.log("ランダム", randomNum);
-    console.log("要素", i);
   }
 };
 shuffle();
@@ -18,12 +15,30 @@ cards.forEach((index) => {
   const card = document.createElement("div");
   card.classList.add("card", "back");
   panel.appendChild(card);
+  card.textContent = cards[index];
   card.addEventListener("click", () => {
     if (flippedCards.length < 2) {
       card.classList.remove("back");
       card.classList.add("flipped");
       flippedCards.push(card);
-      card.textContent = cards[index];
+      setTimeout(checkCards, 1500);
     }
   });
 });
+
+const checkCards = () => {
+  if (flippedCards[0].textContent === flippedCards[1].textContent) {
+    flippedCards[0].classList.add("finish");
+    flippedCards[1].classList.add("finish");
+    console.log(flippedCards[0].textContent, flippedCards[1].textContent);
+    flippedCards = [];
+  } else {
+    flippedCards[0].classList.remove("flipped");
+    flippedCards[1].classList.remove("flipped");
+    flippedCards[0].classList.add("back");
+    flippedCards[1].classList.add("back");
+    console.log(flippedCards[0].textContent, flippedCards[1].textContent);
+    flippedCards = [];
+  }
+  console.log(flippedCards);
+};
